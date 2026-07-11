acedc9bf3006e60e216725b869c3f6ea01dd476f
a0915735362 <a0915735362@smail.ilc.edu.tw>
2026-07-11 16:32:08 +0800

    feat: add paths and intake

--- src/main/java/frc/robot/subsystems/LL4.java
+++ src/main/java/frc/robot/subsystems/LL4.java
@@ -1,351 +1,94 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import frc.robot.LimelightHelpers;
-import frc.robot.LimelightHelpers.PoseEstimate;
-import frc.robot.LimelightHelpers.RawDetection;
+import frc.robot.LimelightHelpers.LimelightResults;
+import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
 import frc.robot.LimelightHelpers.RawFiducial;
 
 public class LL4 extends SubsystemBase {
 
-  private final String name;
-
-  private boolean cachedHasTarget = false;
-  private double cachedTx = 0.0;
-  private double cachedTy = 0.0;
-  private double cachedTa = 0.0;
-  private double cachedTxnc = 0.0;
-  private double cachedTync = 0.0;
-  private double cachedTagId = -1.0;
-  private double cachedHeartbeat = 0.0;
-
-  private RawFiducial[] cachedRawFiducials = new RawFiducial[0];
-  private RawDetection[] cachedRawDetections = new RawDetection[0];
-
-  private int cachedBestFiducialId = -1;
-  private double cachedBestTagDistanceMeters = Double.NaN;
-  private double cachedDistanceMeters = Double.NaN;
-
-  public LL4() {
-    this("limelight-four");
-  }
-
-  public LL4(String name) {
-    if (name == null || name.isEmpty()) {
-      throw new IllegalArgumentException("Limelight name CANNOT be empty");
-    }
-    this.name = name;
-  }
-
-  public boolean hasTarget() {
-    return cachedHasTarget;
-  }
-
-  public double getTX() {
-    return cachedTx;
-  }
-
-  public double getTY() {
-    return cachedTy;
-  }
-
-  public double getTA() {
-    return cachedTa;
-  }
-
-  public double getTXNC() {
+    private final String name;
+    private final Field2d m_field = new Field2d();
     
-    return cachedTxnc;
-  }
+    private static final Transform3d TAG_TO_TARGET_POI = new Transform3d(
+        new Translation3d(0.0, 0.0, 0.0),
+        new Rotation3d()
+    );
 
-  public double getTYNC() {
-    return cachedTync;
-  }
+    private static final Pose3d SHOOTER_POSE_IN_CAM = new Pose3d(
+        new Translation3d(0.0, 0.2, 0.0),
+        new Rotation3d()
+    );
 
-  public double getTagID() {
-    return cachedTagId;
-  }
+    private boolean cachedHasTarget = false;
+    private double cachedTx = 0.0;
+    private double cachedTagId = -1.0;
+    public double cachedDistanceMeters = Double.NaN;
+    private RawFiducial[] cachedRawFiducials = new RawFiducial[0];
 
-  public double getHeartbeat() {
-    return cachedHeartbeat;
-  }
-
-  public void setPipeline(int index) {
-    LimelightHelpers.setPipelineIndex(name, index);
-  }
-
-  public void ledPipelineControl() {
-    LimelightHelpers.setLEDMode_PipelineControl(name);
-  }
-
-  public void ledForceOn() {
-    LimelightHelpers.setLEDMode_ForceOn(name);
-  }
-
-  public void ledForceOff() {
-    LimelightHelpers.setLEDMode_ForceOff(name);
-  }
-
-  public void ledForceBlink() {
-    LimelightHelpers.setLEDMode_ForceBlink(name);
-  }
-
-  public RawFiducial[] getRawFiducials() {
-    return cachedRawFiducials;
-  }
-
-  public RawDetection[] getRawDetections() {
-    return cachedRawDetections;
-  }
-
-  public boolean hasTag(int tagId) {
-    RawFiducial[] tags = getRawFiducials();
-    if (tags == null) return false;
-
-    for (RawFiducial t : tags) {
-      if (t != null && t.id == tagId) return true;
+    public LL4() {
+        this("limelight-four");
     }
-    return false;
-  }
 
-  public boolean hasSpeakerTag() {
-    return hasTag(10) || hasTag(26) || hasTag(21) || hasTag(18)|| hasTag(2)|| hasTag(5);
-  }
+    public LL4(String name) {
+        this.name = name;
+        SmartDashboard.putData("LL4/Field", m_field);
+    }
 
-  public int getBestFiducialId() {
-    return cachedBestFiducialId;
-  }
+    public boolean hasTarget() {
+        return cachedHasTarget;
+    }
 
-  public double getBestGoalYawDeg() {
-    if (!cachedHasTarget) return Double.NaN;
-    return cachedTx;
-  }
+    public double getTX() {
+        return cachedTx;
+    }
 
-  public boolean hasLLTarget() {
-    return hasTarget();
-  }
+    public double getDistanceMeters() {
+        return cachedDistanceMeters;
+    }
 
-  public PoseEstimate getPoseEstimateBlue_MegaTag1() {
-    return LimelightHelpers.getBotPoseEstimate_wpiBlue(name);
-  }
+    @Override
+    public void periodic() {
+        Pose2d botPose = LimelightHelpers.getBotPose2d_wpiBlue(name);
+        m_field.setRobotPose(botPose);
 
-  public PoseEstimate getPoseEstimateBlue_MegaTag2() {
-    return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(name);
-  }
+        LimelightResults results = LimelightHelpers.getLatestResults(name);
 
-  public void setRobotOrientationDeg(
-      double yawDeg,
-      double yawRateDegPerSec,
-      double pitchDeg,
-      double pitchRateDegPerSec,
-      double rollDeg,
-      double rollRateDegPerSec) {
-    LimelightHelpers.SetRobotOrientation(
-        name,
-        yawDeg, yawRateDegPerSec,
-        pitchDeg, pitchRateDegPerSec,
-        rollDeg, rollRateDegPerSec);
-  }
+        if (results != null && results.targets_Fiducials != null && results.targets_Fiducials.length > 0) {
+            cachedHasTarget = true;
+            LimelightTarget_Fiducial bestTarget = results.targets_Fiducials[0];
 
-  public LimelightHelpers.IMUData getIMU() {
-    return LimelightHelpers.getIMUData(name);
-  }
+            cachedTx = bestTarget.tx;
+            cachedTagId = bestTarget.fiducialID;
+            cachedRawFiducials = LimelightHelpers.getRawFiducials(name);
 
-  public void setIMUMode(int mode) {
-    LimelightHelpers.SetIMUMode(name, mode);
-  }
+            Pose3d tagInCam = bestTarget.getTargetPose_CameraSpace();
 
-  public void setIMUAssistAlpha(double alpha) {
-    LimelightHelpers.SetIMUAssistAlpha(name, alpha);
-  }
-
-  public double getBestTagDistanceMeters() {
-    return cachedBestTagDistanceMeters;
-  }
-
-  public double getDistanceMeters() {
-    return cachedDistanceMeters;
-  }
-
-  @Override
-  public void periodic() {
-
-    cachedHeartbeat = LimelightHelpers.getHeartbeat(name);
-    cachedHasTarget = LimelightHelpers.getTV(name);
-    cachedTx = LimelightHelpers.getTX(name);
-    cachedTy = LimelightHelpers.getTY(name);
-    cachedTa = LimelightHelpers.getTA(name);
-    cachedTxnc = LimelightHelpers.getTXNC(name);
-    cachedTync = LimelightHelpers.getTYNC(name);
-    cachedTagId = LimelightHelpers.getFiducialID(name);
-
-    RawFiducial[] fiducials = LimelightHelpers.getRawFiducials(name);
-    cachedRawFiducials = (fiducials != null) ? fiducials : new RawFiducial[0];
-
-    RawDetection[] detections = LimelightHelpers.getRawDetections(name);
-    cachedRawDetections = (detections != null) ? detections : new RawDetection[0];
-
-    cachedBestFiducialId = -1;
-    cachedBestTagDistanceMeters = Double.NaN;
-    cachedDistanceMeters = Double.NaN;
-
-    var results = LimelightHelpers.getLatestResults(name);
-    if (results != null && results.targets_Fiducials != null) {
-      LimelightHelpers.LimelightTarget_Fiducial best = null;
-
-      for (var t : results.targets_Fiducials) {
-        if (t == null) continue;
-        if (best == null || t.ta > best.ta) best = t;
-      }
-
-      if (best != null) {
-        cachedBestFiducialId = (int) best.fiducialID;
-
-        Pose3d tagInCam = best.getTargetPose_CameraSpace();
-        if (tagInCam != null) {
-          cachedBestTagDistanceMeters = tagInCam.getTranslation().getNorm();
-
-          double forward_z = -0.6;
-          double right_x = 0.0;
-          double up_y = -0.7;
-
-          Transform3d tagToPOI = new Transform3d(
-              new Translation3d(right_x, up_y, forward_z),
-              new Rotation3d());
-
-          Pose3d poiInCam = tagInCam.transformBy(tagToPOI);
-          cachedDistanceMeters = poiInCam.getTranslation().getNorm();
+            if (tagInCam != null) {
+                Pose3d poiInCam = tagInCam.transformBy(TAG_TO_TARGET_POI);
+                Pose3d poiInShooter = poiInCam.relativeTo(SHOOTER_POSE_IN_CAM);
+                cachedDistanceMeters = poiInShooter.getTranslation().getNorm();
+            }
+        } else {
+            cachedHasTarget = false;
+            cachedDistanceMeters = Double.NaN;
         }
-      }
+
+        updateDashboard();
     }
 
-    SmartDashboard.putBoolean("LL4/HasTarget", cachedHasTarget);
-    SmartDashboard.putNumber("LL4/tx", cachedTx);
-    SmartDashboard.putNumber("LL4/tagID", cachedTagId);
-    SmartDashboard.putNumber("LL4/Meters", getDistanceMeters());
-
-  }
+    private void updateDashboard() {
+        SmartDashboard.putBoolean("LL4/看到目標", cachedHasTarget);
+        SmartDashboard.putNumber("LL4/目標ID", cachedTagId);
+        SmartDashboard.putNumber("LL4/發射器實際距離_M", cachedDistanceMeters);
+        SmartDashboard.putNumber("LL4/橫向偏移_TX", cachedTx);
+    }
 }
-// package frc.robot.subsystems;
-
-// import edu.wpi.first.math.VecBuilder;
-// import edu.wpi.first.math.Vector;
-// import edu.wpi.first.math.geometry.Pose3d;
-// import edu.wpi.first.math.geometry.Rotation3d;
-// import edu.wpi.first.math.geometry.Transform3d;
-// import edu.wpi.first.math.geometry.Translation3d;
-// import edu.wpi.first.math.numbers.N3;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-// import frc.robot.LimelightHelpers;
-// import frc.robot.LimelightHelpers.PoseEstimate;
-
-// public class LL4 extends SubsystemBase {
-
-//     private final String name;
-
-//     private boolean cachedHasTarget = false;
-//     private double cachedTx = 0.0;
-//     private double cachedTy = 0.0;
-//     private double cachedTagId = -1.0;
-//     private double cachedDistanceMeters = Double.NaN;
-
-//     public LL4() {
-//         this("limelight-four");
-//     }
-
-//     public LL4(String name) {
-//         this.name = name;
-//     }
-
-//     public void updateRobotOrientation(double yawDeg, double yawRate) {
-//         LimelightHelpers.SetRobotOrientation(name, yawDeg, yawRate, 0, 0, 0, 0);
-//     }
-
-
-//     public PoseEstimate getPoseEstimate() {
-//         return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(name);
-//     }
-
-//     public Vector<N3> getEstimationStdDevs(PoseEstimate estimate) {
-//         double xyStdDev = 0.4;
-//         if (estimate.tagCount >= 2) {
-//             xyStdDev = 0.15;
-//         } else if (estimate.avgTagDist > 4.0) {
-//             xyStdDev = 1.2;
-//         }
-//         return VecBuilder.fill(xyStdDev, xyStdDev, 9999999.0);
-//     }
-
-//     public boolean hasTarget() { return cachedHasTarget; }
-//     public double getTX() { return cachedTx; }
-//     public double getTY() { return cachedTy; }
-//     public double getDistanceMeters() { return cachedDistanceMeters; }
-    
-   
-//     public double getID() { return cachedTagId; }
-
-//     @Override
-//     public void periodic() {
-
-//         cachedHasTarget = LimelightHelpers.getTV(name);
-//         cachedTx = LimelightHelpers.getTX(name);
-//         cachedTy = LimelightHelpers.getTY(name);
-//         cachedTagId = LimelightHelpers.getFiducialID(name);
-
-//         // 2. 處理 Fiducial (POI 距離計算)
-//         var results = LimelightHelpers.getLatestResults(name);
-
-//         // 注意：這裡只影響 cachedDistanceMeters，不影響 getPoseEstimate() 的全場定位
-//         if (results != null && results.targets_Fiducials != null && results.targets_Fiducials.length > 0) {
-
-//             LimelightHelpers.LimelightTarget_Fiducial best = null;
-
-//             for (LimelightHelpers.LimelightTarget_Fiducial t : results.targets_Fiducials) {
-//                 // 如果你想讓距離顯示也只針對 10/26，可以加判斷，
-//                 // 但建議保持「選取面積最大者」即可，因為過濾邏輯會寫在 Command 裡。
-//                 if (best == null || t.ta > best.ta) {
-//                     best = t;
-//                 }
-//             }
-
-//             if (best != null) {
-//                 Pose3d tagInCam = best.getTargetPose_CameraSpace();
-//                 if (tagInCam != null) {
-//                     // 2026 POI 補償 (假設值)
-//                     Transform3d tagToPOI = new Transform3d(
-//                         new Translation3d(0.0, -0.7, -0.6),
-//                         new Rotation3d()
-//                     );
-
-//                     Pose3d poiInCam = tagInCam.transformBy(tagToPOI);
-//                     cachedDistanceMeters = poiInCam.getTranslation().getNorm();
-//                 }
-//             }
-//         } else {
-//             cachedDistanceMeters = Double.NaN;
-//         }
-
-//         // 3. Dashboard 更新
-//         SmartDashboard.putBoolean("LL4/HasTarget", cachedHasTarget);
-//         SmartDashboard.putNumber("LL4/Current ID", cachedTagId);
-//         SmartDashboard.putNumber("LL4/POI Distance", cachedDistanceMeters);
-
-//         if (cachedHasTarget) {
-//             var pose = getPoseEstimate();
-//             if (pose != null) {
-//                 // 這裡的 pose 是基於所有可見 Tag 計算的，不會被限制
-//                 SmartDashboard.putNumber("LL4/MegaTag2 X", pose.pose.getX());
-//                 SmartDashboard.putNumber("LL4/MegaTag2 Y", pose.pose.getY());
-//                 SmartDashboard.putNumber("LL4/TagCount", pose.tagCount);
-//             }
-//         }
-//     }
-// }
